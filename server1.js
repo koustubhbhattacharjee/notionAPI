ed25ed29db7d91e21796f33511633191711648f4
koustubh <kbohuastt@gmail.com>
2025-11-24 00:01:35 +0530

    removed comments

--- server1.js
+++ server1.js
@@ -31,24 +31,4 @@ res.json({ correct: response });
 
 api.runQuestions()
 
-//   setInterval(async () => {
-//   try {
-//     await loadQuestions();
-//     await loadTodaysExitTicket();
-//     console.log('Refreshed question caches');
-//   } catch (err) {
-//     console.error('Periodic refresh failed:', err);
-//   }
-// }, 5 * 60 * 1000);
-// Load questions on startup and every 5 minutes
-//loadQuestions();
-// setTimeout(() => {
-//   document.body.innerHTML = `
-//     <div style="text-align:center; margin-top:100px; font-size:2em; color:#555; font-family:system-ui;">
-//       ⏰ Do Now is closed<br><br>
-//       <small>Time's up!</small>
-//     </div>`;
-// }, 10 * 60 * 1000); // exactly 10 minutes
-//loadTodaysExitTicket();
-
 app.listen(process.env.PORT||8000, () => console.log('Server running on http://localhost:8000'));
